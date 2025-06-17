055a4f8e938b66b35c238a865dee823989e5952c
jose <mfdeveloper508@gmail.com>
2025-06-17 02:10:28 -0300

    add swift samples

---
+++ pkg/fednow/models/head_001_001_02/Message.go
@@ -0,0 +1,9 @@
+package head_001_001_02
+
+// Group: FedNow Service
+// Collection: FedNow Service Release 1 - Edition 08/06/2024 (LIVE)
+// Usage Guideline: BusinessApplicationHeader_head.001.001.02
+// Base Message: head.001.001.02
+// Date of publication: 06 August 2024
+// URL: https://www2.swift.com/mystandards/#/mp/mx/_kijaUKEYEeuB-v2uhrZqSw/_qR07WZ5FEeyNX8SYmSuaLA
+// Description: FedNow Service Scope

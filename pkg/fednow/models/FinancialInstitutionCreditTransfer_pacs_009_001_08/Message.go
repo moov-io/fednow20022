8e728b5932f46ac5a89eed21970e186c9a5759b2
jose <mfdeveloper508@gmail.com>
2025-06-17 02:10:28 -0300

    add swift samples

---
+++ pkg/fednow/models/FinancialInstitutionCreditTransfer_pacs_009_001_08/Message.go
@@ -0,0 +1,9 @@
+package FinancialInstitutionCreditTransfer_pacs_009_001_08
+
+// Group: FedNow Service
+// Collection: FedNow Service Release 1 - Edition 08/06/2024 (LIVE)
+// Usage Guideline: FinancialInstitutionCreditTransfer_pacs.009.001.08
+// Base Message: pacs.009.001.08
+// Date of publication: 06 August 2024
+// URL: https://www2.swift.com/mystandards/#/mp/mx/_kijaUKEYEeuB-v2uhrZqSw/_rz7ANZ5FEeyNX8SYmSuaLA
+// Description: FedNow Service Scope
